8ce0bccde84d80c12a739dbdc5b3f0caf9b0878c
Vedhanth <gvedhanth123@outlook.com>
2022-03-28 12:15:48 +0530

    Add files via upload

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -11,67 +11,106 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import to_categoricalpip
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 
-# initialize the initial learning rate, number of epochs to train for,
+
+
+# 1) initialize the initial learning rate, number of epochs to train for,
 # and batch size
+# ---------------------------------------------------------------------------------------------------------------------
+# Initial learning rate
 INIT_LR = 1e-4
-EPOCHS = 20
-BS = 32
 
+# Epoch denotes time here
+EPOCHS = 20
+
+# batch size
+BS = 32
+# ---------------------------------------------------------------------------------------------------------------------
+
+# 2) Saving path in variables
+# ---------------------------------------------------------------------------------------------------------------------
+# saving the path of the dataset in a variable
 DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+
+# There are two folders in the dataset folder and we are saving the names of two subfolders in the dataset folder 
+# into a list for traversal
 CATEGORIES = ["with_mask", "without_mask"]
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# 3) loading the pictures
+# ---------------------------------------------------------------------------------------------------------------------
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
 print("[INFO] loading images...")
 
+# list for storing images
 data = []
+
+# list for storing categories i.e for storing labels [with_mask, without_mask]
 labels = []
 
+# looping through with_mask directory and loading all those images to a list called data
 for category in CATEGORIES:
-    path = os.path.join(DIRECTORY, category)
-    for img in os.listdir(path):
-    	img_path = os.path.join(path, img)
-    	image = load_img(img_path, target_size=(224, 224))
-    	image = img_to_array(image)
-    	image = preprocess_input(image)
 
-    	data.append(image)
-    	labels.append(category)
+    # joining the path
+    path = os.path.join(DIRECTORY, category)
+
+    # storing with_mask images into a list then in second iteration storing without_mask images into a list
+    for img in os.listdir(path):
+
+        # joining the path
+        img_path = os.path.join(path, img)
+
+        # load_img from keras is used to load the image, we give the path to the function
+        image = load_img(img_path, target_size=(224, 224))
+
+        # converting the load_img object to array
+        image = img_to_array(image)
+
+        # then preprocessing the images and storing them into image 
+        image = preprocess_input(image)
+
+        # storing the preprocessed images to the data list
+        data.append(image)
+
+        # atlast storing the category of the image into label list
+        labels.append(category)
+
 
 # perform one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
-
+ 
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
+                                                  test_size=0.20, stratify=labels, random_state=42)
 
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
 
 # load the MobileNetV2 network, ensuring the head FC layer sets are
 # left off
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
+                        input_tensor=Input(shape=(224, 224, 3)))
 
 # construct the head of the model that will be placed on top of the
 # the base model
@@ -89,22 +128,22 @@ model = Model(inputs=baseModel.input, outputs=headModel)
 # loop over all layers in the base model and freeze them so they will
 # *not* be updated during the first training process
 for layer in baseModel.layers:
-	layer.trainable = False
+    layer.trainable = False
 
 # compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 
 # train the head of the network
 print("[INFO] training head...")
 H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+    aug.flow(trainX, trainY, batch_size=BS),
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS)
 
 # make predictions on the testing set
 print("[INFO] evaluating network...")
@@ -116,7 +155,7 @@ predIdxs = np.argmax(predIdxs, axis=1)
 
 # show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
+                            target_names=lb.classes_))
 
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
